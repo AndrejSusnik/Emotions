d62a07a5f08d17fa4a8461c5e94941772f54e947
Andrej Sušnik <susnik.andrej7@gmail.com>
2025-02-22 17:45:18 +0100

    Fix parsing of BMPs

--- src/bmp_parser.py
+++ src/bmp_parser.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 from exit import ExitEx
 from helper_classes import Pair
-import os
+
 
 def parse(filename):
     with Image.open(filename) as img:
@@ -11,8 +11,9 @@ def parse(filename):
             pixel_data = [pixel[:3] for pixel in pixel_data]
         return [pixel_data[i:i + img.width] for i in range(0, len(pixel_data), img.width)]
 
+
 def quantize(pixel):
-    r,g,b = pixel
+    r, g, b = pixel
 
     if r == g and g == b:
         # if r is closer to 255 make it 255 else 0
@@ -20,10 +21,12 @@ def quantize(pixel):
 
     return pixel
 
+
 def parse_bmp(filename):
     pixel_data = parse(filename)
     pixel_data = [[quantize(pixel) for pixel in row] for row in pixel_data]
-    raw = np.array([np.array([tuple(pixel) for pixel in row]) for row in pixel_data])
+    raw = np.array([np.array([tuple(pixel) for pixel in row])
+                   for row in pixel_data])
 
     walls = []
     exits = []
@@ -34,27 +37,21 @@ def parse_bmp(filename):
 
     print(uniq)
 
-    tmp_exit = ExitEx(0)
     for y, row in enumerate(pixel_data):
         for x, pixel in enumerate(row):
             if pixel == (0, 0, 0):
                 walls.append(Pair(x, y))
             elif pixel == (0, 0, 255):
-                if (tmp_exit.is_empty()):
-                    tmp_exit.id = len(exits)
-                    tmp_exit.add_point(Pair(x, y))
-                else:
-                    if (tmp_exit.points[-1].x == x) or (tmp_exit.points[-1].y == y):
-                        tmp_exit.add_point(Pair(x, y))
-                    else:
-                        exits.append(tmp_exit)
-                        tmp_exit = ExitEx(len(exits), [])
-                        tmp_exit.add_point(Pair(x, y))
-    
-    if not tmp_exit.is_empty():
-        exits.append(tmp_exit)
-                    
-    return exits, walls, Pair(len(pixel_data[0]), len(pixel_data)), raw 
+                exit = ExitEx(len(exits), [Pair(x, y)])
+
+                for old_exit in exits:
+                    # if the distance of centers is less than 1 then add to siblings
+                    if (old_exit.center - exit.center).norm() <= 1:
+                        old_exit.add_sibling(exit)
+                exits.append(exit)
+
+    return exits, walls, Pair(len(pixel_data[0]), len(pixel_data)), raw
+
 
 def write_bmp(array, filename):
     img = Image.new('RGB', (len(array[0]), len(array)))
